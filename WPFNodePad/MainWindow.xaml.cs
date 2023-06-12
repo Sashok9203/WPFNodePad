59fc33e78aceabd1a4264f0bcc8b2dd8afff720d
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-06-12 23:54:43 +0300

    Update

--- WPFNodePad/MainWindow.xaml.cs
+++ WPFNodePad/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -26,32 +27,19 @@ namespace WPFNodePad
     /// </summary>
     public partial class MainWindow : Window
     {
+        ViewModel VM;
         bool saved = true;
         string? curentPath = null;
         public MainWindow()
         {
             InitializeComponent();
-            for (double i = 6; i < 78; i += 2)
-                fontSize.Items.Add(i);
-            fontColor.ItemsSource  = typeof(Colors).GetProperties();
-            fontColor.SelectedIndex = 7;
-            copy.IsEnabled = false;
-            paste.IsEnabled = Clipboard.ContainsText();
+            VM = new(userText);
+            DataContext = VM;
         }
 
                
 
-        private void UserTextChanged(object sender, TextChangedEventArgs e)
-        {
-            saved = false;
-            save.IsEnabled = !saved;
-            rowsCount.Content = userText.Text.Count(n => n == '\n');
-            simbolsCount.Content = userText.Text.Count(n=>!Char.IsWhiteSpace(n));
-            wordsCount.Content = getWordsСount();
-            undo.IsEnabled = userText.CanUndo;
-            redo.IsEnabled = userText.CanRedo;
-            sAll.IsEnabled = userText.Text.Length > 0;
-        }   
+       
 
 
         private void Operation(object sender, RoutedEventArgs e)
@@ -62,21 +50,21 @@ namespace WPFNodePad
             switch (command)
             {
                 case "newFile":
-                    if (!saved && MessageBox.Show("Do you wont save current text?", "Save", MessageBoxButton.YesNo) == MessageBoxResult.Yes) SaveDocument();
+                    if (!saved && MessageBox.Show("Do you wont save current text?", "Save", MessageBoxButton.YesNo) == MessageBoxResult.Yes) ;
 
                     curentPath = null;
                     userText.Text = string.Empty;
                     saved = true;
                     break;
-                case "openFile": LoadDocument(); break;
-                case "saveFile": SaveDocument(); break;
-                case "saveFileAs": SaveDocument(true); break;
+                case "openFile":; break;
+                case "saveFile": ; break;
+                case "saveFileAs": /*SaveDocument(true)*/; break;
                 case "exit": Close(); break;
                 case "undo": userText.Undo();  break;
                 case "redo": userText.Redo(); break;
-                case "copy": userText.Copy(); paste.IsEnabled = true; break;
+                case "copy": userText.Copy();  break;
                 case "paste": userText.Paste(); break;
-                case "cut": userText.Cut(); paste.IsEnabled = true; break;
+                case "cut": userText.Cut();  break;
                 case "selectAll": userText.SelectAll(); break;
                 case "deSelectAll": userText.Select(userText.Text.Length, 0); break;
                 case "about": MessageBox.Show("Simple Text Editor", "About program"); break;
@@ -84,43 +72,7 @@ namespace WPFNodePad
             }
         }
  
-        private void SaveDocument(bool newPath = false)
-        {
-            string tmpPath = curentPath;
-            if (newPath || curentPath == null)
-            {
-                SaveFileDialog sfd = new()
-                {
-                    Filter = "Text Files (*.txt)|*.txt"
-                };
-
-                if (sfd.ShowDialog() == true)
-                {
-                    if (!newPath) curentPath = sfd.FileName;
-                    tmpPath = sfd.FileName;
-                }
-                else return;
-            }
-            File.AppendAllText(tmpPath, userText.Text); ;
-            saved = true;
-            save.IsEnabled = !saved;
-        }
-
-        private void LoadDocument()
-        {
-            if (!saved && MessageBox.Show("Do you wont save current text?", "Save", MessageBoxButton.YesNo) == MessageBoxResult.Yes) SaveDocument();
-            OpenFileDialog sfd = new()
-            {
-                Filter = "Text Files (*.txt)|*.txt"
-            };
-
-            if (sfd.ShowDialog() == true) curentPath = sfd.FileName;
-            else return;
-
-            userText.Text = File.ReadAllText(curentPath);
-            saved = true;
-            save.IsEnabled = !saved;
-        }
+        
 
         private int getWordsСount()
         {
@@ -139,24 +91,9 @@ namespace WPFNodePad
         }
 
        
-        private void FontToggleButtons(object sender, RoutedEventArgs e)
-        {
-            ToggleButton? button = sender as ToggleButton;
-            switch (button?.Name)
-            {
-                case "italicFont":
-                    userText.FontStyle = button.IsChecked == true? FontStyles.Italic : FontStyles.Normal;
-                    break;
-                case "boldFont":
-                    userText.FontWeight = button.IsChecked == true ? FontWeights.Bold : FontWeights.Normal;
-                    break;
-                case "underlineFont":
-                  userText.TextDecorations = button.IsChecked == true ? TextDecorations.Underline : null;
-                    break;
-            }
-        }
+       
 
-        private void SelectionChanged(object sender, RoutedEventArgs e) => copy.IsEnabled = userText.SelectedText.Length != 0;
+       
         
     }
 }
---
+++ WPFNodePad/ViewModel.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.Win32;
+using PropertyChanged;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WPFNodePad
+{
+    [AddINotifyPropertyChangedInterface]
+    internal class ViewModel
+    {
+        private bool saved = true;
+        string? curentPath = null;
+        private readonly TextBox textBox;
+        private readonly RelayCommand copy;
+        private readonly RelayCommand paste;
+        private readonly RelayCommand cut;
+        private readonly RelayCommand undo;
+        private readonly RelayCommand redo;
+        private readonly RelayCommand selectAll;
+        private readonly RelayCommand deSelectAll;
+        private readonly RelayCommand fontStyle;
+        private readonly RelayCommand fontWeight;
+        private readonly RelayCommand tDecor;
+        private readonly RelayCommand newFile;
+        private readonly RelayCommand about;
+        private readonly RelayCommand openFile;
+        private readonly RelayCommand saveFile;
+        private readonly RelayCommand saveFileAs;
+
+        public IEnumerable<PropertyInfo> FontColors => typeof(Colors).GetProperties();
+        public double[] FontSize => Enumerable.Range(6, 78)
+            .Where(n => n % 2 == 0)
+            .Select(n => (double)n)
+            .ToArray();
+        public string Text { get; set; }
+
+        [DependsOn("Text")]
+        public int RowsCount
+        {
+            get
+            {
+                saved = false;
+                return Text.Count(n => n == '\n');
+            }
+        }
+
+        [DependsOn("Text")]
+        public int CharsCount => Text.Count(n => !Char.IsWhiteSpace(n));
+
+        [DependsOn("Text")]
+        public int WordsCount => getWordsСount();
+
+        public ICommand Copy => copy; 
+        public ICommand Paste => paste;
+        public ICommand Cut => cut;
+        public ICommand Undo => undo;
+        public ICommand Redo => redo;
+        public ICommand SelectAll => selectAll;
+        public ICommand DeSelectAll => deSelectAll;
+        public ICommand FStyle => fontStyle;
+        public ICommand FWeight => fontWeight;
+        public ICommand TextDecor => tDecor;
+        public ICommand NewFile => newFile;
+        public ICommand About => about;
+        public ICommand OpenFile => openFile;
+        public ICommand SaveFile => saveFile;
+        public ICommand SaveFileAs => saveFileAs;
+
+
+        public ViewModel(TextBox tBox)
+        {
+            textBox = tBox;
+            textBox.TextDecorations = null;
+            Text = textBox.Text;
+            copy = new((o) => textBox.Copy(), (o) => textBox.SelectionLength > 0);
+            paste = new((o) => textBox.Paste(), (o) => Clipboard.ContainsText()) ;
+            cut = new((o) => textBox.Cut(), (o) => textBox.SelectionLength > 0);
+            undo = new((o) => textBox.Undo(), (o) => textBox.CanUndo);
+            redo = new((o) => textBox.Redo(), (o) => textBox.CanRedo);
+            selectAll = new((o) => textBox.SelectAll(), (o) => Text.Length > 0 && textBox.SelectionLength!= Text.Length);
+            deSelectAll = new((o) => textBox.SelectionLength = 0, (o) => textBox.SelectionLength > 0);
+            fontStyle = new((o) => { textBox.FontStyle = textBox.FontStyle == FontStyles.Normal ? FontStyles.Italic : FontStyles.Normal;});
+            fontWeight = new((o) => { textBox.FontWeight = textBox.FontWeight == FontWeights.Normal ? FontWeights.Bold : FontWeights.Normal; });
+            tDecor = new((o) => { textBox.TextDecorations = textBox.TextDecorations == null ? TextDecorations.Underline : null;});
+            newFile = new((o) =>  nFine() , (o) => (curentPath != null) || (Text != string.Empty));
+            about = new((o) => MessageBox.Show("Simple Text Editor", "About program"));
+            openFile = new((o) => LoadDocument());
+            saveFile = new((o) => SaveDocument(), (o) => ! saved);
+            saveFileAs = new((o) => SaveDocument(true));
+        }
+
+        private void nFine()
+        {
+            if (!saved && MessageBox.Show("Do you wont save current text?", "Save", MessageBoxButton.YesNo) == MessageBoxResult.Yes) SaveDocument();
+            curentPath = null;
+            Text = string.Empty;
+            saved = true;
+        }
+
+        private void SaveDocument(bool newPath = false)
+        {
+            string tmpPath = curentPath;
+            if (newPath || curentPath == null)
+            {
+                SaveFileDialog sfd = new()
+                {
+                    Filter = "Text Files (*.txt)|*.txt"
+                };
+
+                if (sfd.ShowDialog() == true)
+                {
+                    if (!newPath) curentPath = sfd.FileName;
+                    tmpPath = sfd.FileName;
+                }
+                else return;
+            }
+            File.AppendAllText(tmpPath, Text); ;
+            saved = !newPath;
+        }
+
+        private void LoadDocument()
+        {
+            if (!saved && MessageBox.Show("Do you wont save current text?", "Save", MessageBoxButton.YesNo) == MessageBoxResult.Yes) SaveDocument();
+            OpenFileDialog sfd = new()
+            {
+                Filter = "Text Files (*.txt)|*.txt"
+            };
+
+            if (sfd.ShowDialog() == true) curentPath = sfd.FileName;
+            else return;
+
+            Text = File.ReadAllText(curentPath);
+            saved = true;
+           
+        }
+
+        private int getWordsСount()
+        {
+            int words = 0;
+            bool end = true;
+            foreach (var ch in Text)
+            {
+                if (!Char.IsPunctuation(ch) && !Char.IsWhiteSpace(ch))
+                {
+                    if (end) words++;
+                    end = false;
+                }
+                else end = true;
+            }
+            return words;
+        }
+    }
+}
